c92c5d8e9314371d072d08f7a1e9ce143a6f4972
Igor <romanigor.conect@gmail.com>
2022-08-31 20:52:08 +0200

    2.Возведите число А в натуральную степень B используя цикл

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Возведите число А в натуральную степень B используя цикл
+void Fillnumber(int x, int y)
+{
+ int count = 1;
+ double pow = 1;
+ while(count<=y)
+{
+    pow = Math.Pow(x,count);
+    count++;
+    Console.WriteLine(pow);
+}
+}
+
+Console.WriteLine("Enter the number A:");
+int number = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine();
+Console.WriteLine("Enter the number B:");
+int number2 = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine();
+
+Fillnumber(number,number2);
