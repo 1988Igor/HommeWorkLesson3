0f2f2e637c0cc6a64d8ad5f8ea00ef66dc93b9d9
Igor <romanigor.conect@gmail.com>
2022-08-31 21:27:27 +0200

    6.Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран

---
+++ Task6/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+void PrintArray(int[] array)
+{
+
+array = new int[8];
+for(int i = 0; i<array.Length; i++)
+{
+    Console.Write( array[i] );
+}
+Console.Write("/");
+
+for(int i = 0; i<array.Length; i++)
+{
+    array[i] = array[i]+1;
+    Console.Write(array[i]);
+}
+
+}
+
+int []myArray = new int[8];
+PrintArray(myArray);
