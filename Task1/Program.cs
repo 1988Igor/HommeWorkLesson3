c9e98156309302c6012cd47d919fee8ca084c3b2
Igor <romanigor.conect@gmail.com>
2022-08-31 20:30:10 +0200

    1.Найти кубы чисел от 1 до N

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Найти кубы чисел от 1 до N
+void Fillnumber (int n)
+{
+int count  = 1;
+while (count<=n)
+{
+
+double result = Math.Pow(count,3);
+Console.WriteLine($"{count}*{count}*{count}* = {result};");
+count++;
+}
+
+}
+
+Console.WriteLine("Enter the number N :");
+int number  = int.Parse(Console.ReadLine()??"0");
+Fillnumber(number);
+
