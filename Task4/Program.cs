97ce0f6f346207459545e4f131d0eb1f5dc1474a
Igor <romanigor.conect@gmail.com>
2022-08-31 21:20:15 +0200

    4.//Написать программу вычисления произведения чисел от 1 до N

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿//Написать программу вычисления произведения чисел от 1 до N
+
+int []array = {2,3,5,4,8};
+int mul = 1;
+for (int i =0; i<array.Length;i++)
+{
+   
+                    mul *= array[i];
+            
+            Console.WriteLine( $"{mul}");
+                       
+}
+
